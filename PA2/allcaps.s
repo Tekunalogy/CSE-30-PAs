e3e4d332250f2c3af4bbe66046c4a00b11fe23a8
Kunal Singla <tekunalogy@gmail.com>
2020-08-24 19:56:10 -0700

    Added comments and finished README file

--- PA2/allcaps.s
+++ PA2/allcaps.s
@@ -3,61 +3,54 @@
 * Author: Kunal Singla
 * UserId: CSE30S220EQ
 * Date: 08/23/2020
-* Sources of help: Lecture Slides, Piazza
+* Sources of help: Lecture Slides, Piazza, Tutors
 */
 
-@ Describe the hardware to the assembler
+@ Hardware Description
 	.arch	armv6
 	.cpu    cortex-a53
 	.syntax unified
 
-@ Declare any external functions
-	.extern putchar
-	.global main
+@ Functions
+	.extern putchar                 @ function to write a char to stdout
+	.global main                    @ defining global main function
 
-@ Define any constants
-	.equ 	FP_OFFSET, 4
-	.equ 	EXIT_SUCCESS, 0
-	.equ 	CAPS_SUB_FACTOR, 32
-	.equ 	NULL_TERMINATOR, 0x0
-	.equ	LOWER_CASE_A, 0x61
-	.equ	LOWER_CASE_Z, 0x7A
+@ Constants
+	.equ 	FP_OFFSET, 4            @ fp offset in simple frame
+	.equ 	EXIT_SUCCESS, 0         @ value for successful ending of program
+	.equ 	CAPS_SUB_FACTOR, 32     /* factor for converting lower case to
+                                           upper case */
+	.equ 	NULL_TERMINATOR, 0x0    /* Null terminator character '\0' for
+                                           detecting end of string */
+	.equ	LOWER_CASE_A, 0x61      @ ASCII value in hex for lower case a
+	.equ	LOWER_CASE_Z, 0x7A      @ ASCII value in hex for lower case z
 
-@ Start data segment where you will define your string
+@ Data segment
 	.section .rodata
-mesg:	.asciz	"Hello World!"
+mesg:	.asciz	"hello world!"          @ mesg label for string to manipulate
 
 	.text
-	.type	main, %function
-
-/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!! IMPORTANT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  */
-/* You MUST name the string you will convert as "mesg" (without the quotes) */
-/* Also, do NOT have any spaces between "mesg" and ":" when defining your   */ 
-/* string. It should look like "mesg:" exactly.                             */
-
-@ Start text segment and begin writing your function
+	.type	main, %function         @ indication that main is a function
 
 /*
+* Function Name: main
+* Function prototype: main:
+* Description: The main function runs the program to modify any lower case
+*              letters in a string into uppercase.
+* Side Effects: prints "lower-cased" string to standard out
+* Error Conditions: None
+* Return Value: None
 * Registers used:
 *   <register> - <use> -- <description of what the value represents>
 * example:
-*   r0 - arg 1 -- number of values to add together
-*   r1 - arg 2 -- starting value to start adding from
-*   r2 - local var -- keeps track of loop index
-*
-* Stack variables:
-*   < name> - <fp offset> -- <description of what the value represents>
-* example:
-*   sum - [fp - 8] -- holds the sum of values
+*   r0 - char -- stores current char of string
+*   r4 - string -- stores the string pointer
 */
-
-@r0-r3 temp registers
-@r4+ non-temp registers
 
 main:
 	/* PROLOGUE */
-	push	{fp, lr}
-	add	fp, sp, FP_OFFSET
+	push	{fp, lr}                @ stack frame register save
+	add	fp, sp, FP_OFFSET       @ locate our frame pointer
 
 	/* START OF MAIN PROGRAM */
 	ldr	r4, =mesg		@ load mesg into r4 (non-temp) register
@@ -72,7 +65,7 @@ loop:
 	sub	r0, r0, CAPS_SUB_FACTOR @ between 'a' and 'z', make uppercase
 next_char:
 	bl	putchar			@ print char to terminal
-	add	r4, r4, 1		@ increment register "index"
+	add	r4, r4, 1		@ increment register "index" by 1
 	ldrb	r0, [r4]		@ load next char (byte) into r1 (temp)
 	cmp	r0, NULL_TERMINATOR	@ check if end of string
 	bne	loop			@ go to loop again if not end of string
@@ -80,8 +73,8 @@ next_char:
 
 epilogue:
 	/* EPILOGUE */
-	mov	r0, EXIT_SUCCESS
-	sub	sp, fp, FP_OFFSET
-	pop	{fp, lr}
-	bx	lr
+	mov	r0, EXIT_SUCCESS        @ set return to EXIT_SUCCESS
+	sub	sp, fp, FP_OFFSET       @ restore stack frame top
+	pop	{fp, lr}                @ remove stack frame and restore
+	bx	lr                      @ return to calling function
 .end
